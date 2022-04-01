5092dd3f4d7288af558a6626c91e0fc1a9e634ff
devnafiz <nafiz.2011@yahoo.com>
2022-04-01 23:44:16 +0600

    create employee

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -4,11 +4,36 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Employee;
 
 class ApiController extends Controller
 {
      public function createEmployee(Request $request){
 
+         $validateData =$request->validate([
+
+             'name'=>'required',
+             'email'=>'required|email|unique:employees',
+             'phone_no'=>'required',
+             'gender' =>'required',
+             'age' =>'required'
+
+         ]);
+
+         $employee = new Employee();
+         $employee->name =$request->name;
+         $employee->email =$request->email;
+          $employee->phone_no =$request->phone_no;
+
+          $employee->gender =$request->gender;
+          $employee->age =$request->age;
+          $employee->save();
+          return response()->json([
+
+           "status" => 1,
+           'message' =>'Employee created sucessfully'
+
+          ]);
 
      }
 
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('list-employees',[ApiController::class,'ListEmployees']);
+Route::get('single-employee/{id}',[ApiController::class,'getSingleEmployee']);
+Route::post('add-employee',[ApiController::class,'createEmployee']);
+Route::put('update-employee/{id}',[ApiController::class,'updateEmployee']);
+Route::delete('delete-employee/{id}',[ApiController::class,'deleteEmployee']);
+
+
